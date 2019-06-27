3a964414e4ecdf40ad294750a3b9dd029959dffa
olesyadovbush <olesyadovbush@gmail.com>
2019-06-27 14:11:45 +0300

    Updates

---
+++ precache-manifest.b7e6df775a6547391f3a1448c709c24c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d95ce0ee96a2873abbbbcf23bc44767",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "0305d061c3ee862f2354",
+    "url": "/todo-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f62a0e4b1c9f0648e14e",
+    "url": "/todo-app/static/css/main.80d9206c.chunk.css"
+  },
+  {
+    "revision": "0305d061c3ee862f2354",
+    "url": "/todo-app/static/js/2.9354a162.chunk.js"
+  },
+  {
+    "revision": "f62a0e4b1c9f0648e14e",
+    "url": "/todo-app/static/js/main.2ebeab1b.chunk.js"
+  },
+  {
+    "revision": "f7728c30f6339eef41ed",
+    "url": "/todo-app/static/js/runtime~main.eca9ff85.js"
+  },
+  {
+    "revision": "0959a1de893419ca0e866a7e6c4d9628",
+    "url": "/todo-app/static/media/orange.0959a1de.jpg"
+  }
+]);
